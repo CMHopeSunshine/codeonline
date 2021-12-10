cda9bc918ea8b5e9addf84551f3b7d30238816b6
CMHopeSunshine <63870437+CMHopeSunshine@users.noreply.github.com>
2021-12-11 00:50:48 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+import hoshino
+
+HELP_MSG = '''
+1.[#code [语言] (-i) (输入)] :在线运行代码
+'''
+sv = hoshino.Service('code', bundle='详细', help_=HELP_MSG)
+
+@sv.on_prefix('#code')
+async def code(bot, ev):
+  code = ev.message.extract_plain_text().split()
+  res = await run(code)
+  await bot.send(ev, res, at_sender=True)
