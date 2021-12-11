1962a8e97ebb5246c02867ca84260f650709dae4
CMHopeSunshine <63870437+CMHopeSunshine@users.noreply.github.com>
2021-12-11 12:48:08 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,12 +3,12 @@ from . import *
 from .run import *
 
 HELP_MSG = '''
-1.[#code [语言] (-i) (输入)] :在线运行代码
+1.[#code#[语言]#(-i)#(输入)#代码] :在线运行代码
 '''
 sv = hoshino.Service('code', bundle='详细', help_=HELP_MSG)
 
 @sv.on_prefix('#code')
 async def code(bot, ev):
-  code = ev.message.extract_plain_text().split()
+  code = ev.message.extract_plain_text().split('#')
   res = await run(code)
   await bot.send(ev, res, at_sender=True)
